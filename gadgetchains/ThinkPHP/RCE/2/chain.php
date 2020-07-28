98b4ff65377def0e36fe190e5c8f466dab777e63
AFKL-CUIT <1565972452@qq.com>
2020-07-28 12:52:25 +0800

    fix ThinkPHP\RCE2

--- gadgetchains/ThinkPHP/RCE/2/chain.php
+++ gadgetchains/ThinkPHP/RCE/2/chain.php
@@ -4,7 +4,7 @@ namespace GadgetChain\ThinkPHP;
 
 class RCE2 extends \PHPGGC\GadgetChain\RCE
 {
-    public static $version = '<= 6.0.3';
+    public static $version = '6.0.0 <= 6.0.3';
     public static $vector = '__destruct';
     public static $author = 'AFKL';
 
