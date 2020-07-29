4ea5ce3eb67fab6cb807baed46c77e63538ef0ae
AFKL-CUIT <1565972452@qq.com>
2020-07-29 19:00:55 +0800

    add the informations of Yii/RCE2

--- gadgetchains/Yii/RCE/2/chain.php
+++ gadgetchains/Yii/RCE/2/chain.php
@@ -7,6 +7,9 @@ class RCE2 extends \PHPGGC\GadgetChain\RCE
     public static $version = '2.0.0 <= 2.0.13';
     public static $vector = '__destruct';
     public static $author = 'AFKL';
+	public static $informations = '
+        This chain requires both gii and debug extensions. (Doesn\'t matter if it\'s on or not.)
+    ';
 
     public function generate(array $parameters)
     {
